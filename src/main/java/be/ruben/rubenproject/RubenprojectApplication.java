67406eb0106dde0b0af578388243556fcf51e393
Ruben Muanza <galliano1986@live.nl>
2022-02-09 23:33:40 +0100

    bezig

---
+++ src/main/java/be/ruben/rubenproject/RubenprojectApplication.java
@@ -0,0 +1,13 @@
+package be.ruben.rubenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubenprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubenprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ruben/rubenproject/RubenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ruben.rubenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubenprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
