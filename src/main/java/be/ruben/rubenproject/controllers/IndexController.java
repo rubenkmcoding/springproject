c387d835e608fa703654ffe80acdeabe42485d0e
Ruben Muanza <galliano1986@live.nl>
2022-02-11 11:33:29 +0100

    indexcontroller

--- src/main/java/be/ruben/rubenproject/controllers/IndexController.java
+++ src/main/java/be/ruben/rubenproject/controllers/IndexController.java
@@ -1,4 +1,22 @@
 package be.ruben.rubenproject.controllers;
+import org.springframework.boot.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
+
+@RestController
+@RequestMapping("/") // associeert de controller met een URL in je website
 public class IndexController {
+    @GetMapping // verwerkt de bij behorende Get request, de method wordt door spring opgeroepen.
+    public String index(){
+        var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
+        return "<!doctype html><html><title><hallo></title><body>Goede " + morgenOfMiddag + "</body></html>";
+    }
+
+
 }
